519548b3f171728bfa57f6b731a4143a74d9dce1
ogallagher <owengall@icloud.com>
2021-08-17 12:27:53 -0400

    com.javafx package javadoc str

---
+++ src/main/java/com/fxgraph/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * An interactive diagramming and graphing library for JavaFX.
+ * 
+ * @author <a href="https://github.com/sirolf2009">sirolf2009</a>
+ * @author <a href="https://github.com/ogallagher">ogallagher</a> (javadoc)
+ * 
+ * @see <a href="https://stackoverflow.com/questions/30679025/graph-visualisation-like-yfiles-in-javafx">stackoverflow question</a>
+ * 
+ */
+package com.fxgraph;
